99263f1c57821f5d084ae7a5bfc4a6c54c697e23
almalee24 <flores.almalee@gmail.com>
2020-12-03 11:52:07 +0200

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial=>{
+    let words = tutorial.split(' ')
+    return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  })
 }
